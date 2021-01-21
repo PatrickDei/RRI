2f99d9057907d7fe96e98ddfa092e3fd5b3bdd8f
Patrick Deisinger <patrick.deis@gmail.com>
2021-01-21 12:31:37 +0100

    "Car" is moving and added a smooth camera

---
+++ RRIGame/Assets/Scripts/MovementController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform car;
+    public float speed = 17;
+
+
+    Vector3 rotationRight = new Vector3(0, 150, 0);
+    Vector3 rotationLeft = new Vector3(0, -150, 0);
+
+    Vector3 forward = new Vector3(0, 0, 1);
+    Vector3 backward = new Vector3(0, 0, -1);
+
+    // Initialize
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        car = gameObject.GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(backward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationRight);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationLeft);
+        }
+
+    }
+}
