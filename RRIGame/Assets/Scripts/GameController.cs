d916a634897f8ccd2ca98536eb9e2232e848f3ac
Patrick Deisinger <patrick.deis@gmail.com>
2021-01-22 22:26:55 +0100

    Scene upload

--- RRIGame/Assets/Scripts/GameController.cs
+++ RRIGame/Assets/Scripts/GameController.cs
@@ -6,9 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-
     public Button restartButton;
-    // Start is called before the first frame update
+
     void Start()
     {
         restartButton.onClick.AddListener(restartLevel);
