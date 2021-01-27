d13c0aefb53c454d895fb519a2c15502d70f18bd
Patrick Deisinger <patrick.deis@gmail.com>
2021-01-27 17:59:46 +0100

    Comment buggy code

--- RRIGame/Assets/Scripts/LapComplete.cs
+++ RRIGame/Assets/Scripts/LapComplete.cs
@@ -21,10 +21,10 @@ public class LapComplete : MonoBehaviour
 	void OnTriggerEnter()
 	{
 		Debug.Log(MilliDisplay.GetComponent<Text>().text.Substring(0, 1));
-		if ((Int32.Parse(SecondDisplay.GetComponent<Text>().text.Trim('.', ':')) >= LapTimeManager.SecondCount
+		/*if ((Int32.Parse(SecondDisplay.GetComponent<Text>().text.Trim('.', ':')) >= LapTimeManager.SecondCount
 			&& Int32.Parse(MinuteDisplay.GetComponent<Text>().text.Trim(':', '.')) >= LapTimeManager.MinuteCount)
 			|| Int32.Parse(MilliDisplay.GetComponent<Text>().text.Trim(',', ':', '.')) == 0)
-		{
+		{*/
 			if (LapTimeManager.SecondCount <= 9)
 			{
 				SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
@@ -45,7 +45,7 @@ public class LapComplete : MonoBehaviour
 
 			MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
 			Debug.Log(MilliDisplay.GetComponent<Text>().text);
-		}
+		//}
 
 		LapTimeManager.MinuteCount = 0;
 		LapTimeManager.SecondCount = 0;
